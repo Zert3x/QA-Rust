c577e9a2009249328c342146764a321e1ab76fe5
Taptiive <bastian.v2745@gmail.com>
2021-08-15 15:58:05 -0400

    Fixed stream write unexpected crash changing ip and added new HWID maker

--- src/utils.rs
+++ src/utils.rs
@@ -1,20 +1,8 @@
+use machineid_rs::{Encryption, IdBuilder};
 
-use sysinfo::SystemExt;
-
-pub fn get_id() -> std::string::String {
-    let system = sysinfo::System::new_all();
-
-    let disk_count = system.get_disks().len();
-    let cpu_count = system.get_physical_core_count().unwrap_or(4);
-    let ram_count = system.get_total_memory();
-    let os_name = system
-        .get_long_os_version()
-        .unwrap_or_else(|| String::from(""));
-    let host_name = system.get_host_name().unwrap_or_else(|| String::from(""));
-    let payload = format!(
-        "{}-{}-{}-{}-{}",
-        disk_count, cpu_count, ram_count, os_name, host_name
-    );
-    let dig = md5::compute(payload);
-    hex::encode(dig.0.iter().map(|x| *x as char).collect::<String>())
+pub(crate) fn get_id() -> std::string::String {
+    let mut builder = IdBuilder::new(Encryption::SHA256);
+    builder.add_drive_serial().add_cpu_cores().add_machine_name()
+    .add_os_name().add_system_id();
+    builder.build("QuartzAuth")
 }
