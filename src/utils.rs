9808e99ef186de07b8d3a3bfd36ef9f0c967c746
Taptiive <bastian.v2745@gmail.com>
2021-08-18 15:18:44 -0400

    Updated crate version, fixed linux support and replaced error checking with .is_err

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,7 @@ use machineid_rs::{Encryption, IdBuilder};
 
 pub(crate) fn get_id() -> std::string::String {
     let mut builder = IdBuilder::new(Encryption::SHA256);
-    builder.add_drive_serial().add_cpu_cores().add_machine_name()
-    .add_os_name().add_system_id();
+    builder.add_cpu_cores().add_machine_name()
+    .add_os_name().add_system_id().add_cpu_id();
     builder.build("QuartzAuth")
 }
